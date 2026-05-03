85faec531d490e9fa13394959423f362aff6f110
Emerson-1982 <emersonfreitas1982@gmail.com>
2026-05-03 16:31:58 -0400

    Delete backend directory

--- backend/database/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE localizacoes (
-  id SERIAL PRIMARY KEY,
-  veiculo VARCHAR(20),
-  latitude DOUBLE PRECISION,
-  longitude DOUBLE PRECISION,
-  status VARCHAR(10),
-  data_hora TIMESTAMP
-);
